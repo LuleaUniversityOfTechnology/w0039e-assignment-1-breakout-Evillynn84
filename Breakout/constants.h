6996b20eb2003902e22894395313205b6f6fe187
Evillynn84 <linn.johansson.leinonen@gmail.com>
2024-12-03 12:50:00 +0100

    Reworked folder structure to omit extra project

---
+++ Breakout/constants.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "game.h"
+
+const int DISPLAY_WIDTH = 640;
+const int DISPLAY_HEIGHT = 360;
+const int DISPLAY_SCALE = 2;
+const float ballSpeed = 5;
+
+
+
+enum ObjectType {
+	TYPE_BALL, TYPE_BRICK
+};
+
